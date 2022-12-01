df07470a5f6438d8f4eba1d4a5128fad2e55ce81
Lev Stambler <levstamb@gmail.com>
2022-11-30 20:32:34 -0500

    first commit

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>About Me</title>
+	<meta name="description" content="About Me" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Who is Lev?</h1>
+	More to come!JJ
+	<p>
+		Lev is a cool dude
+	</p>
+</div>
---
+++ src/routes/research/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>About Me</title>
+	<meta name="description" content="About Me" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>My Research</h1>
+
+	<p>
+		M
+	</p>
+</div>
